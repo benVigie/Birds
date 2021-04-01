fef70ad1e1fa0b378a275403a0c8efee22ec6bcf
decanus <7621705+decanus@users.noreply.github.com>
2021-04-01 21:11:15 +0200

    const

--- sharedConstants.js
+++ sharedConstants.js
@@ -1,25 +1,25 @@
 // Define all constants usefull by the server and the client
-var constant = {
+const constant = {
 
-  SERVER_PORT:              4242,
-  SOCKET_PORT:              1337,
-  SOCKET_ADDR:              'http://localhost',
+  SERVER_PORT: 4242,
+  SOCKET_PORT: 1337,
+  SOCKET_ADDR: 'http://localhost',
 
-  SCREEN_WIDTH:             900,
-  SCREEN_HEIGHT:            768,
-  FLOOR_POS_Y:              672,
-  LEVEL_SPEED:              0.3,
-  TIME_BETWEEN_GAMES:       5000,
+  SCREEN_WIDTH: 900,
+  SCREEN_HEIGHT: 768,
+  FLOOR_POS_Y: 672,
+  LEVEL_SPEED: 0.3,
+  TIME_BETWEEN_GAMES: 5000,
 
-  BIRD_WIDTH:               52,
-  BIRD_HEIGHT:              40,
+  BIRD_WIDTH: 52,
+  BIRD_HEIGHT: 40,
 
   // Pipe constants
-  PIPE_WIDTH:               100,
-  DISTANCE_BETWEEN_PIPES:   380,
-  MIN_PIPE_HEIGHT:          60,
-  MAX_PIPE_HEIGHT:          630,
-  HEIGHT_BETWEEN_PIPES:     150
+  PIPE_WIDTH: 100,
+  DISTANCE_BETWEEN_PIPES: 380,
+  MIN_PIPE_HEIGHT: 60,
+  MAX_PIPE_HEIGHT: 630,
+  HEIGHT_BETWEEN_PIPES: 150
 };
 
 // To be use by the server part, we have to provide the object with exports
