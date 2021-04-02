143cfbbef133341f25b59f3b94c2bf171e07b250
Jeff Reiner <theundeadghost@gmail.com>
2021-04-02 11:40:43 +0200

    Adding in mustache over jade for templating

--- server.ts
+++ server.ts
@@ -1,29 +1,44 @@
 import express from "express";
 import path from "path";
+import mustacheExpress from "mustache-express";
 
-var Const = require("./sharedConstants").constant;
-var game = require("./game_files/game");
+// import game from "./game_files/game";
+
+const Const = {
+  SERVER_PORT: 4242,
+  SOCKET_PORT: 1337,
+  SOCKET_ADDR: "http://localhost",
+};
 
 const app = express();
 
-// all environments
-app.set("port", Const.SERVER_PORT);
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "mustache");
+app.engine("mustache", mustacheExpress());
 
+/**
+ * Middleware
+ */
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+/**
+ * Routes
+ */
 app.get("/birds", (req, res) =>
   res.render("birds", {
-    title: "Birds.js",
-    wsAddress: Const.SOCKET_ADDR + ":" + Const.SOCKET_PORT,
+    wsAddress: `${Const.SOCKET_ADDR}:${Const.SOCKET_PORT}`,
   })
 );
+
+/**
+ * Setup
+ */
+app.set("port", Const.SERVER_PORT);
 
 app.listen(app.get("PORT"), () => {
   console.log(`🐦 [server]: listening at ${app.get("PORT")}`);
 });
 
-game.startServer();
+// game.startServer();
